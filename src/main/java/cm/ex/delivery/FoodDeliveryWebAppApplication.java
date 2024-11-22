96ddaabaa4cbb972dc447e46c5003c6a0f333fe7
kritesh2nd <kritesh2nd@gmail.com>
2024-11-22 08:37:21 +0545

    [1.6] database populated, user login and signup works

--- src/main/java/cm/ex/delivery/FoodDeliveryWebAppApplication.java
+++ src/main/java/cm/ex/delivery/FoodDeliveryWebAppApplication.java
@@ -6,7 +6,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodDeliveryWebAppApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(FoodDeliveryWebAppApplication.class, args);
 		System.out.println("Food Delivery Web Application");
+
 	}
 }
+
+/*
+drop database delivery; create database delivery; use delivery;
+
+
+user authentication
+
+
+crete new acccount
+signup - yesma jwt token aauxa
+
+
+login
+signup
+signup ma authority
+security - jwt token
+error handeling for login and signup
+
+*/
+
+
